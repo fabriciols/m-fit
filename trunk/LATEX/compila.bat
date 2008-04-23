5756f143c93b9c400ec799c714774c177233887e
mizu.jp <mizu.jp@e7c4d9e1-0347-0410-b93a-375c3849f0f1>
2008-04-23 22:18:11 +0000

    Update pra compilar as bibliotecas

--- trunk/LATEX/compila.bat
+++ trunk/LATEX/compila.bat
@@ -1,5 +1,7 @@
 cd "C:\TCC\SRC\LATEX"
 "C:\Arquivos de programas\MiKTeX 2.7\miktex\bin\pdflatex.exe" "Master.tex"
+"C:\Arquivos de programas\MiKTeX 2.7\miktex\bin\pdfbib.exe" "Master"
+"C:\Arquivos de programas\MiKTeX 2.7\miktex\bin\pdflatex.exe" "Master.tex"
 "C:\Arquivos de programas\MiKTeX 2.7\miktex\bin\pdflatex.exe" "Master.tex"
 taskkill /IM "FOXITR~1.EXE"
 Master.pdf
