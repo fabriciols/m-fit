3b73c0c942d1ccc0597153dd497f7d0667b200e5
linux.fabricio <linux.fabricio@e7c4d9e1-0347-0410-b93a-375c3849f0f1>
2008-05-08 04:30:24 +0000

    Nao usar, em desenvolvimento ainda.

--- trunk/LATEX/compila.bat
+++ trunk/LATEX/compila.bat
@@ -1,5 +1,20 @@
 cd "C:\Documents and Settings\fabriciols\Desktop\TCC\SRC\LATEX"
-set FOLDER=C:\Program Files\MiKTex 2.7\miktex\bin\
+set FOLDER_EN=C:\Program Files
+set FOLDER_BR=C:\Arquivos de Programas
+set FOLDER_BASE=MiKTex 2.7\miktex\bin\
+
+if exist "%FOLDER_BR%" goto br
+if exist "%FOLDER_EN%" goto en
+
+:br
+set FOLDER=%FOLDER_BR%\%FOLDER_BASE%
+goto main
+
+:en
+set FOLDER=%FOLDER_EN\%FOLDER_BASE%
+goto main
+
+:main
 "%FOLDER%\pdflatex.exe" "Master.tex"
 "%FOLDER%\bibtex.exe" "Master"
 "%FOLDER%\pdflatex.exe" "Master.tex"
