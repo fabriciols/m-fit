0d7f045f2f7c02e18ff0bc3b55313b5f5104f288
linux.fabricio <linux.fabricio@e7c4d9e1-0347-0410-b93a-375c3849f0f1>
2008-05-08 04:21:44 +0000

    Arrumado essa porra.

--- trunk/LATEX/compila.bat
+++ trunk/LATEX/compila.bat
@@ -1,7 +1,8 @@
-cd "C:\TCC\SRC\LATEX"
-"C:\Arquivos de programas\MiKTeX 2.7\miktex\bin\pdflatex.exe" "Master.tex"
-"C:\Arquivos de programas\MiKTeX 2.7\miktex\bin\pdfbib.exe" "Master"
-"C:\Arquivos de programas\MiKTeX 2.7\miktex\bin\pdflatex.exe" "Master.tex"
-"C:\Arquivos de programas\MiKTeX 2.7\miktex\bin\pdflatex.exe" "Master.tex"
+cd "C:\Documents and Settings\fabriciols\Desktop\TCC\SRC\LATEX"
+set FOLDER=C:\Program Files\MiKTex 2.7\miktex\bin\
+"%FOLDER%\pdflatex.exe" "Master.tex"
+"%FOLDER%\bibtex.exe" "Master"
+"%FOLDER%\pdflatex.exe" "Master.tex"
+"%FOLDER%\pdflatex.exe" "Master.tex"
 taskkill /IM "FOXITR~1.EXE"
 Master.pdf
