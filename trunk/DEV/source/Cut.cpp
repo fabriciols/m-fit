eddcd3d72f2cd89df1673dd2970f9d5c1eb0e39c
mizu.jp <mizu.jp@e7c4d9e1-0347-0410-b93a-375c3849f0f1>
2008-07-04 16:18:29 +0000

    Implementando DetectTransitions do corte.
    isso é Upcast?

--- trunk/DEV/source/Cut.cpp
+++ trunk/DEV/source/Cut.cpp
@@ -5,3 +5,29 @@
 #include "../include/Transition.h"
 #include "../include/DetectTransitions.h"
 #include "../include/Cut.h"
+
+/************************************************************************
+* Função que faz a detecção das transições do tipo corte.
+*************************************************************************
+* param (E): int type => tipo de detecção de transição a ser feita.
+* 								 0 -> todas
+* 								 1 -> só cortes
+*				 Video* vdo => vídeo onde será feita a detecção
+*************************************************************************
+* return : Lista de transições.
+*************************************************************************
+* Histórico:
+* 04/07/08 - Thiago Mizutani
+* Criação.
+************************************************************************/
+
+Transition* DetectTransitions::detectTransitions(int type, Video* vdo)
+{ 
+	// Upcast!!!
+	VisualRythim* vr = 0;
+	IplImage* visualRythim = 0;
+	
+	visualRythim = vr->createVR(vdo);
+
+	
+}
