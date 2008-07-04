2a201a5e43069d2a2aa6edb14a1314b7bef53ce9
mizu.jp <mizu.jp@e7c4d9e1-0347-0410-b93a-375c3849f0f1>
2008-07-04 16:19:17 +0000

    implementando createVR
    nao mexam... se for mexer facam a parte de vcs

--- trunk/DEV/source/VisualRythim.cpp
+++ trunk/DEV/source/VisualRythim.cpp
@@ -3,3 +3,44 @@
 #include "../include/Histogram.h"
 #include "../include/Frame.h"
 #include "../include/VisualRythim.h"
+
+/************************************************************************
+* Função que cria o Ritmo Visual por amostragem de um vídeo.
+*************************************************************************
+* param (E): Video* vdo => vídeo do qual será montado o ritmo visual.
+*************************************************************************
+* return : Ritmo Visual do vídeo.
+*************************************************************************
+* Histórico:
+* 04/07/08 - Thiago Mizutani
+* Criação.
+************************************************************************/
+
+IplImage* VisualRythim::createVR(Video* vdo)
+{
+	Frame* vr = 0;
+	
+	/**
+	 * Visto que os frames de um mesmo video nao tem suas dimensoes
+	 * alteradas, frame->heigth e frame->width são fixos para todos
+	 * os objetos, então só altero o frame->data.
+	**/	
+	Frame frame = new Frame(cvCaptureFrame(vdo));
+	
+	vr = cvCreateImage(frame->)
+	
+	while(!frame)
+	{
+		// Pego a diagonal do frame.
+		vr->getDiagonal(frame);
+	
+		// Pego um novo frame.	
+		frame->data = cvCaptureFrame(vdo); // trocar por getNextFrame
+
+		// Se não houver mais frames, chegou ao fim do video.
+		if (!frame)
+			break;
+	}
+
+	return (vr->data);
+}
