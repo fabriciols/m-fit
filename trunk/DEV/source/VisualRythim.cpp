3349e2192a778120de46a164ebc060b75498b199
mizu.jp <mizu.jp@e7c4d9e1-0347-0410-b93a-375c3849f0f1>
2008-07-29 19:42:22 +0000

    Implementada a criação do ritmo visual por histograma.
    Mudança do nome das funcoes getMaxIdx e getMinIdx para getMaxLuminance e getMinLuminance 
    
    Retirada de alguns parâmetros inúteis e imprestáveis.

--- trunk/DEV/source/VisualRythim.cpp
+++ trunk/DEV/source/VisualRythim.cpp
@@ -11,6 +11,8 @@
 
 #include "../include/Log.h"
 
+#include <stdio.h>
+
 /************************************************************************
 * Função que cria o Ritmo Visual por amostragem de um vídeo.
 *************************************************************************
@@ -19,6 +21,8 @@
 * return : Ritmo Visual do vídeo.
 *************************************************************************
 * Histórico:
+* 29/07/08 - Thiago Mizutani
+* Retirando coisas obsoletas.
 * 06/07/08 - Thiago Mizutani
 * Implementação do processo que monta as colunas do ritmo visual atraves
 * da extracao dos pixels da diagonal de um frame.
@@ -29,55 +33,29 @@ Frame* VisualRythim::createVR(Video* vdo)
 {
 
 	Frame *frameVR;
-
 	Frame* frame;
+	Frame *frameAux;
 
 	Color *color;
-	IplImage* imgVR;
-
-	Frame *frameAux;
 
 	int lum;
 
 	double width;
 	double heigth;
-	int f = 0; // nro do frame corrente.
 	int x = 0; // Coluna que está sendo montada no RV.
-	double max_frames; // Numero total de frames do video.
-	int diagonal;
 
 	Log::writeLog("%s :: param: Video[%x]", __FUNCTION__, vdo);
 
 	color = new Color();
 
-	// Pego o primeiro frame do video.
+	// Pego o primeiro frame do vídeo e sua diagonal	
 	width = vdo->getFramesWidth();
 	heigth = vdo->getFramesHeight();
-	max_frames = vdo->getFramesTotal(); // Numero total de frames do video.
-
-	diagonal = (int)cvRound(sqrt( pow(width,2) + pow(heigth,2)));
-
-	/**
-	 *  Crio a imagem do RV com largura = ao nro de frames do video (cada
-	 *  frame representa 1 coluna do RV) e altura = largura do frame.
-   **/
-
-	imgVR = cvCreateImage(cvSize((int)1, (int)diagonal), 8, 1);
-
 	frame = vdo->getNextFrame();
-
 	frameVR = new Frame(frame->getDiagonal());
 
-	f = 0;
-
-	for (f=0 ; f< max_frames ; f++)
+	while(!frame)
 	{
-
-		frame = vdo->getNextFrame();
-
-		// Se não houver mais frames, chegou ao fim do video.
-		if (!frame)
-			break;
 
 		Log::writeLog("%s :: Convert2Gray [%x]", __FUNCTION__, frame);
 
@@ -87,16 +65,89 @@ Frame* VisualRythim::createVR(Video* vdo)
 		// Pego a diagonal (pixel por pixel) e ploto este pixel na coluna f do RV.
 		frameAux = frame->getDiagonal();
 
+		/** Utilizando sobrecarga de operador para concatenar uma nova coluna ao frame
+		 * do Ritmo Visual.
+		**/
+
 		*frameVR += *frameAux;
 
 		// Desaloca os temporarios
 		delete frameAux;
 		delete frame;
+		
+		frame = vdo->getNextFrame();
 	}
 
-	Log::writeLog("%s :: frame[%x] max_frames[%.f] diagonal[%d]", __FUNCTION__, imgVR, max_frames, diagonal);
+	Log::writeLog("%s :: max_frames[%.f]", __FUNCTION__, vdo->getFramesTotal());
 
 	Log::writeLog("%s :: end ", __FUNCTION__);
 
 	return (frameVR);
+}
+
+/************************************************************************
+* Função que cria o Ritmo Visual por histograma de um vídeo.
+*************************************************************************
+* param (E): Video* vdo => vídeo do qual será montado o ritmo visual.
+*************************************************************************
+* return : Ritmo Visual por histograma do vídeo.
+*************************************************************************
+* Histórico:
+* 29/07/08 - Thiago Mizutani
+* Criação.
+************************************************************************/
+
+int* VisualRythim::createVRH(Video* vdo)
+{
+	Frame* frameVRH = 0;	// Ritmo visual por histograma
+	Frame* frame = 0; 
+	Frame* diagonal = 0;
+	
+	Histogram* histogram = 0;	
+
+	double totalFrames = vdo->getFramesTotal();
+	int posic = 0;
+
+	/**
+	 * Crio um array com o numero de posicoes = ao nro de frames do video
+	 * para guardar o valor máximo de cada histograma que será gerado.
+	 * Obs.: Será gerado 1 histograma por frame.
+	**/
+	int* hist;
+
+	hist = (int*)malloc(sizeof(int)*cvRound(totalFrames));
+	memset(hist,'0',cvRound(totalFrames));
+
+	// Pego o primeiro frame e sua diagonal
+
+	frame = vdo->getNextFrame();	
+	diagonal = new Frame(frame->getDiagonal());
+	
+	while(!frame)
+	{
+		// Gero o histograma da diagonal do frame corrente
+		histogram = new Histogram(diagonal->data);	
+		
+		// Guardo o maior valor de luminancia do histograma.	
+		hist[posic] = histogram->getMaxLuminance();	
+	
+		Log::writeLog("%s :: hist[%d] = %d", __FUNCTION__, posic, hist[posic]);
+
+		/**
+		 * Deleto os objetos criados anteriormente para desalocamento de
+		 * memoria.
+		**/
+		delete histogram;
+		delete frame;
+		delete diagonal;
+		
+		// Pego o proximo frame e sua diagonal
+		frame = vdo->getNextFrame();
+		diagonal = frame->getDiagonal();
+
+		posic++;
+	}
+
+	// Retorno o array com os valores do RVH
+	return (hist);
 }
