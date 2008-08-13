f58d1f312d34a8a11b458086417329e3e3458f22
mizu.jp <mizu.jp@gmail.com>
2008-08-13 12:11:40 +0000

    Video.h:
    Comentei a função getFramePos que não serve pra mais nada.
    E agora faz uso do getCurrentPosition.
    
    DetectTransitions.h:
    
    Incluído o parametro int type na função detect transitions.
    
    Cut.h:
    
    Incluída a função detectTransitions.

--- DEV/include/Video.h
+++ DEV/include/Video.h
@@ -1,32 +1,29 @@
-/**
- *Classe responsável por fazer
- *a manipulação de um vídeo.
- */
+// Classe responsável por fazer a manipulação de um vídeo.
 class Video
 {
- 
+
 	/**
 	 *Armazena o nome do vídeo
 	 */
 	private:
 		char name[300+1];
-	 
-	/**
-	 *Armazena o endereço do vídeo 
-	 *carregado no sistema
-	 */
+
+		/**
+		 *Armazena o endereço do vídeo 
+		 *carregado no sistema
+		 */
 		char path[300+1];
-	 
-	 
-	/**
-	 *Armazena o tamanho do vídeo.
-	 */
-//		Time length;
-	 
-	/**
-	 *Armazena o codec do video.
-	 *
-	 */
+
+
+		/**
+		 *Armazena o tamanho do vídeo.
+		 */
+		//		Time length;
+
+		/**
+		 *Armazena o codec do video.
+		 *
+		 */
 		double codec;
 
 		double framesHeight;
@@ -39,101 +36,91 @@ class Video
 
 		Time timePos;
 
-	/**
-	 *Armazena o sistema de cores que
-	 *se encontra o vídeo.
-	 */
+		/**
+		 *Armazena o sistema de cores que
+		 *se encontra o vídeo.
+		 */
 		double systemColor;
-	 
-	/**
-	 *Armazena a taxa de frames
-	 *por segundo do vídeo.
-	 *
-	 */
-		double fps;
-	 
-	/**
-	 *Nó da lista de tomadas.
-	 */
-	//	Take take;
 
-	/**
-	 *Função que abre o vídeo
-	 *e atualiza as variáveis do objeto.
-	 */
+		/**
+		 *Armazena a taxa de frames
+		 *por segundo do vídeo.
+		 *
+		 */
+		double fps;
+
+		/**
+		 *Nó da lista de tomadas.
+		 */
+		//	Take take;
+
+		/**
+		 *Função que abre o vídeo
+		 *e atualiza as variáveis do objeto.
+		 */
 		CvCapture* open(char *vdoSrc); 
-	 
+
 	public:
 
 		char* getName();
 		char* getPath();
-	/**
-	 *Armazena o vídeo carregado.
-	 */
+		/**
+		 *Armazena o vídeo carregado.
+		 */
 		CvCapture* data;
 
 		Video(char *filename_cy);
 
-	/**
-	 *Método responsável por obter
-	 *o próximo frame em relação à
-	 *posição atual do vídeo.
-	 */
+		/**
+		 *Método responsável por obter
+		 *o próximo frame em relação à
+		 *posição atual do vídeo.
+		 */
 		Frame* getNextFrame();
-	 
-	/**
-	 *Método responsável por obter
-	 *o frame anterior em relação à
-	 *posição atual do vídeo.
-	 */
+
+		/**
+		 *Método responsável por obter
+		 *o frame anterior em relação à
+		 *posição atual do vídeo.
+		 */
 		Frame* getPreviousFrame();
 
-	/**
-	 *Método responsável em 
-	 *posicionar o ponteiro do vídeo
-	 *na posição desejada, a qual é
-	 *parecida como parâmetro da 
-	 *função.
-	 */
+		/**
+		 * Método responsável em posicionar o ponteiro do vídeo na posição desejada,
+		 * a qual é parecida como parâmetro da função.
+		 **/
 		int seekFrame(unsigned long posFrame);
-	 
-	 
-	/**
-	 *Função que fecha o vídeo.
-	 *1 - sucesso.
-	 *0 - erro.
-	 *
-	 */
+
+
+		/**
+		 *Função que fecha o vídeo.
+		 *1 - sucesso.
+		 *0 - erro.
+		 *
+		 */
 		int close();
-	 
-	/**
-	 *Obtém o frame atual.
-	 */
-	Frame* getCurrentFrame();
 
-	double getFramesTotal();
-	 
-	/**
-	 *Obtêm a posição corrente do
-	 *ponteiro do vídeo.
-	 */
-	int getCurrentPosition();
-	 
-	/**
-	 *Processo que escreve no arquivo.
-	 *É nele onde são aplicados
-	 *realmente os efeitos provenientes
-	 *da edição do vídeo.
-	 *
-	 *retorna:
-	 *
-	 * 1 - Sucesso.
-	 *<0 - Erro.
-	 */
-	int write();
+		/**
+		 *Obtém o frame atual.
+		 */
+		Frame* getCurrentFrame();
 
-	double getFramesWidth();
-	double getFramesHeight();
-	void updatePos();
-	double getFramePos();
+		double getFramesTotal();
+
+		// Obtêm a posição corrente do ponteiro do vídeo.
+		double getCurrentPosition();
+
+		/**
+		 * Processo que escreve no arquivo. É nele onde são aplicados
+		 * realmente os efeitos provenientes da edição do vídeo.
+		 * retorna:
+		 * 1 - Sucesso.
+		 * 0 - Erro.
+		 **/
+		int write();
+
+		double getFramesWidth();
+		double getFramesHeight();
+		void updatePos();
+		//	double getFramePos();
 };
