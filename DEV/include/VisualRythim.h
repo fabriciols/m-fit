b1070321e9d0d3374380e4678f5553e2aaebec39
Mizu.jp <mizu.jp@gmail.com>
2008-09-02 17:54:40 +0000

    Retirada do wideScreen

--- DEV/include/VisualRythim.h
+++ DEV/include/VisualRythim.h
@@ -23,8 +23,4 @@ class VisualRythim
 		 *gráfico do ritmo visual.
 		 */
 		float calcDerivatives();
-
-		// Remove as tarjas de wide screen, se estas existirem.
-		void removeWide(Frame* visualRythim);
-
 };
