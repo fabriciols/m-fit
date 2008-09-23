0e5bc5c4156a4f2e276602a19277d652d986e730
linux.fabricio <linux.fabricio@e7c4d9e1-0347-0410-b93a-375c3849f0f1>
2008-09-23 11:56:59 +0000

    Classe que define o escopo de um projeto.

---
+++ trunk/DEV/source/Project.cpp
@@ -0,0 +1,18 @@
+#include "cv.h"
+#include <QImage>
+#include "highgui.h"
+
+#include "../include/Time.h"
+
+#include "../include/Histogram.h"
+#include "../include/Frame.h"
+#include "../include/Video.h"
+#include "../include/VisualRythim.h"
+#include "../include/Project.h"
+
+#include "../include/Log.h"
+
+Project::Project(char *filename_cy)
+{
+	// Abre um projeto ja existente
+}
