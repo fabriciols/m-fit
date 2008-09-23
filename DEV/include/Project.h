092baf8a41e812d89a0c4d25ca716b5313657a60
Fabrício Lopes de Souza <linux.fabricio@gmail.com>
2008-09-23 11:56:59 +0000

    Classe que define o escopo de um projeto.

---
+++ DEV/include/Project.h
@@ -0,0 +1,16 @@
+class Project
+{
+	private:
+		Video *vdo;
+		Frame *frame;
+		char *path_cy;
+		char *filename_cy;
+
+	public:
+		Project(Video *vdo);
+		Project(char *filename_cy);
+		Video *getVideo();
+		Frame *getFrame();
+		char  *getPath();
+		char  *getFileName();
+};
