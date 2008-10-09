aa354c5bc24eee9bc65168d7207eb57260e14bd5
ivanmachida <ivanmachida@e7c4d9e1-0347-0410-b93a-375c3849f0f1>
2008-10-09 05:10:04 +0000

    - retirando libxml2 Vamo usa XML do QT 4.4.3 ele tem XPath nessa nova versão

--- trunk/DEV/include/Xml.h
+++ trunk/DEV/include/Xml.h
@@ -1,14 +1,14 @@
 #ifndef XML_INCLUDE
 #define XML_INCLUDE
 
+#include <QIODevice>
+#include <QXmlInputSource>
+#include <QtXml>
+#include <QXmlReader>
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <libxml/xmlreader.h>
-#include <libxml/xmlwriter.h>
-#include <libxml/parser.h>
-#include <libxml/tree.h>
-#include <libxml/parser.h>
+
 /**
  *Classe responsável por detectar
  *o dissolve.
@@ -16,15 +16,14 @@
 class Xml
 {
 	private:
-		xmlDocPtr doc;
-		xmlNodePtr tree, subtree, root;
+		QDomDocument doc;
 		char *xmlName;
 
 	public:
-		xmlDocPtr openXml(char *);
-		xmlDocPtr createXml(char *);
-		xmlNodePtr findNextTag(xmlNodePtr, int, char *, char *);
-		xmlNodePtr writeTag(char *, char *, int , xmlDocPtr, xmlNodePtr);
-		int closeXml(xmlDocPtr);
+		int openXml(char *);
+		int createXml(char *);
+		int findNextTag(char *);
+		int writeTag(char *, char *);
+		int closeXml();
 };
 #endif
