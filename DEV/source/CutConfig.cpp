aed9ddd15c1459d38f04bd05e42bc43fd2ed201e
Mizu.jp <mizu.jp@gmail.com>
2008-10-16 02:25:15 +0000

    evento botao

--- DEV/source/CutConfig.cpp
+++ DEV/source/CutConfig.cpp
@@ -27,13 +27,42 @@ CutConfig::CutConfig(QDialog *parent) : QDialog(parent)
 {
 	ui.setupUi(this);
 
-	if (currentProject->getUserThreshold())
+	/**
+	 * Verifico se existem valores definidos pelo usuário. Se sim, utilizo 
+	 * estes valores como valor inicial, senão utilizo o valor padrão.
+	**/
+	if (currentProject->getUserThreshold()) // Limiar de corte
 		ui.spinPercentage->setValue(currentProject->getUserThreshold());
 
-	if (currentProject->getUserFirstCanny())
+	if (currentProject->getUserFirstCanny()) // Limiar mínimo do Canny
 		ui.spinFirstCanny->setValue(currentProject->getUserFirstCanny());
 
-	if (currentProject->getUserLastCanny())
+	if (currentProject->getUserLastCanny()) // Limiar máximo do Canny
 		ui.spinLastCanny->setValue(currentProject->getUserLastCanny());
 
 }
+
+
+void CutConfig::on_buttonOk_clicked()
+{
+	int userCutThreshold = ui.spinPercentage->value();
+	int userFirstCanny = ui.spinFirstCanny->value();
+	int userLastCanny = ui.spinLastCanny->value();
+
+	if ( !userCutThreshold )
+		Interface::alertUser();
+	else if ( userCutThreshold != DEFAULT_CUT_THRESHOLD )
+		currentProject->setUserThreshold(userCutThreshold);
+
+	if ( !userFirstCanny )
+		Interface::alertUser();
+	else if ( userFirstCanny != DEFAULT_FIRST_CANNY )
+		currentProject->setUserFirstCanny(userFirstCanny);
+
+	if ( !userLastCanny )
+		Interface::alertUser();
+	else if ( userLastCanny != DEFAULT_LAST_CANNY )
+		currentProject->setUserLastCanny(userLastCanny);
+
+}
+
