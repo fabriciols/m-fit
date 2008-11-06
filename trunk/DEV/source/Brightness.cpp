bbf0e6f5b2b63b62756479db930ae637fb23440f
Mizu.jp <Mizu.jp@e7c4d9e1-0347-0410-b93a-375c3849f0f1>
2008-11-06 01:26:37 +0000

    deletando

--- trunk/DEV/source/Brightness.cpp
+++
@@ -1,8 +0,0 @@
-#include "cv.h"
-
-#include <QImage>
-
-#include "../include/Histogram.h"
-#include "../include/Frame.h"
-#include "../include/Effect.h"
-#include "../include/Brightness.h"
--- trunk/DEV/source/Contrast.cpp
+++
@@ -1,5 +0,0 @@
-#include "cv.h"
-#include <QImage>
-#include "../include/Histogram.h"
-#include "../include/Frame.h"
-#include "../include/Contrast.h"
