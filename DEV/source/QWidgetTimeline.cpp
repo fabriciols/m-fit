924f87010163ad7f88b9261936eadce8ee9997c1
Fabrício Lopes de Souza <linux.fabricio@gmail.com>
2008-11-09 19:34:15 +0000

    Comentando os fontes.

--- DEV/source/QWidgetTimeline.cpp
+++ DEV/source/QWidgetTimeline.cpp
@@ -18,19 +18,38 @@ extern Project* currentProject;
 extern VideoPlayer* vdo_player;
 extern Interface* Interface_ui;
 
-QWidgetTimeline::QWidgetTimeline(QWidget *parent) : QWidget(parent)
-{ 
-}
+/************************************************************************
+* Construtor
+*************************************************************************
+* param (E): QWidget *parent - Widget parente.
+*************************************************************************
+* Histórico
+* 29/09/08 - Fabricio Lopes de Souza
+* Criação.
+************************************************************************/
+QWidgetTimeline::QWidgetTimeline(QWidget *parent) : QWidget(parent) { }
 
+/************************************************************************
+* Trata o evento de pressionar o mouse.
+*************************************************************************
+* param (E): QMouseEvent* - Evento do mouse.
+*************************************************************************
+* Histórico
+* 29/09/08 - Fabricio Lopes de Souza
+* Criação.
+************************************************************************/
 void QWidgetTimeline::mousePressEvent(QMouseEvent *event)
 {
+	// Pega os pontos
 	QPoint point(this->pos());
 	QPoint pointEvent(event->pos());
 	QRect  rect(this->geometry());
 
+	// Se o player estiver rodando, ignora
 	if (vdo_player->isRunning())
 		return;
 
+	// Se nao tiver video carregado, ignora
 	if (currentProject->getVideo() == 0x0)
 		return;
 
@@ -39,9 +58,12 @@ void QWidgetTimeline::mousePressEvent(QMouseEvent *event)
 			point.x(), point.y(),
 			pointEvent.x(), pointEvent.y());
 
+	// Se o click foi no intervalo tratado.
+	// Lembrando que a area visivel começa no 9 pixel
 	if (pointEvent.x() >= 9 && pointEvent.x() <= rect.width() - 9)
 	{
 		Video *vdo;
+		Frame *frame;
 
 		int posFrame;
 		int x;
@@ -53,23 +75,25 @@ void QWidgetTimeline::mousePressEvent(QMouseEvent *event)
 
 		vdo = currentProject->getVideo();
 
-		//posFrame = cvRound(SIZE_SEC_FRAME*vdo->getFPS())*x / SIZE_FRAME_TIMELINE;
+		// Converte a posicao clicada para a posicao no Video.
 		posFrame = currentProject->TimelinePosToFrame(x);
 
 		Log::writeLog("%s :: setting frame to pos [%d]", __FUNCTION__, posFrame);
 
+		// Posiciona o ponteiro do Video na posição clicada.
 		vdo->seekFrame(posFrame);
-
-		Frame *frame;
 
 		frame = vdo->getCurrentFrame();
 
+		// Atualiza o player com a posicao clicada
 		vdo_player->updatePlayer(frame);
 		vdo_player->updateHist(frame);
 
 		// Vamos mostrar a transicao referente ao ponto que o usuario clicou
 		transitionID = currentProject->getTransitionByPos(pointEvent.x());
 
+		// Se o ponto clicado contiver uma transicao
+		// Desenha o Header da timeline
 		if (transitionID >= 0 && transitionID < currentProject->transitionList.size())
 		{
 			Interface_ui->clearTransitionHeader();
@@ -80,13 +104,18 @@ void QWidgetTimeline::mousePressEvent(QMouseEvent *event)
 		delete frame;
 
 	}
-	else
-	{
-		return;
-	}
-
 }
 
+/************************************************************************
+* Trata o evento de DROP na timeline.
+* Aplica o efeito arrastado no intervalo da transição escolhida.
+*************************************************************************
+* param (E): QDropEvent* - Evento de drop
+*************************************************************************
+* Histórico
+* 29/09/08 - Fabricio Lopes de Souza
+* Criação.
+************************************************************************/
 void QWidgetTimeline::dropEvent(QDropEvent *event)
 {
 	Effect *effect = 0x0;
@@ -96,8 +125,7 @@ void QWidgetTimeline::dropEvent(QDropEvent *event)
 	long start = -1;
 	long end = 0;
 
-
-	// Pego os pontos onde o efeito vai ser aplicado
+	// Procuro na lista de transicoes a transicao que esta selecionada.
 	for (i = 0; i < currentProject->transitionList.size() ; i++)
 	{
 		transition = &currentProject->transitionList.at(i);
@@ -134,6 +162,7 @@ void QWidgetTimeline::dropEvent(QDropEvent *event)
 		end = (long)vdo->getFramesTotal();
 	}
 
+	// Define qual efeito foi arrastado
 	switch(getItemByEvent(event))
 	{
 		case EFFECT_COLOR:
@@ -153,22 +182,38 @@ void QWidgetTimeline::dropEvent(QDropEvent *event)
 
 	}
 
+	// Se nao aplicou nenhum efeito, retorna
 	if (effect == 0x0)
 		return;
 
+	// Empilha o efeito
 	currentProject->effectList.push_back(effect);
 
-	Interface_ui->clearTransitionHeader();
+	// Atualiza a Tree
 	Interface_ui->updateEffectTree();
+
+	// Atualiza o cabecalho da Timeline
+	Interface_ui->clearTransitionHeader();
 
 	// Atualiza o cursor
 	vdo_player->updateCurrentFrame();
 
-	// Pode salvar
+	// Habilita salvar
 	Interface_ui->enableSaveButton();
 
 }
 
+/************************************************************************
+* Trata o evento de DragEnter.
+* Este evento ocorre quando um objeto que está sendo arrastado entra
+* no range de tratamento da timeline.
+*************************************************************************
+* param (E): QDragEnterEvent* - Evento
+*************************************************************************
+* Histórico
+* 29/09/08 - Fabricio Lopes de Souza
+* Criação.
+************************************************************************/
 void QWidgetTimeline::dragEnterEvent(QDragEnterEvent * event)
 {
 	int ret_i = 0;
@@ -177,6 +222,7 @@ void QWidgetTimeline::dragEnterEvent(QDragEnterEvent * event)
 	if (vdo_player->frameTimeline == 0)
 		return;
 
+	// Seleciona a transicao de acordo com as coordenadas do evento
 	ret_i = selectDropTransition(event->pos());
 
 	// Se modificamos a timeline, aceita o evento
@@ -187,6 +233,17 @@ void QWidgetTimeline::dragEnterEvent(QDragEnterEvent * event)
 
 }
 
+/************************************************************************
+* Trata o evento de DragMove.
+* Este evento ocorre quando um objeto que está sendo arrastado, que já
+* está dentro do range da timeline, se movimenta.
+*************************************************************************
+* param (E): QDragMoveEvent * - Evento
+*************************************************************************
+* Histórico
+* 29/09/08 - Fabricio Lopes de Souza
+* Criação.
+************************************************************************/
 void QWidgetTimeline::dragMoveEvent(QDragMoveEvent *event)
 {
 	int ret_i = 0;
@@ -195,6 +252,7 @@ void QWidgetTimeline::dragMoveEvent(QDragMoveEvent *event)
 	if (vdo_player->frameTimeline == 0)
 		return;
 
+	// Seleciona a transicao de acordo com as coordenadas do evento
 	// Se tem o CTRL pressionado, nao apagamos o anterior
 	if (event->keyboardModifiers() & Qt::ControlModifier)
 	{
@@ -212,18 +270,41 @@ void QWidgetTimeline::dragMoveEvent(QDragMoveEvent *event)
 	return;
 }
 
+/************************************************************************
+* Trata o evento de DragLeave.
+* Este evento ocorre quando um objeto que está sendo arrastado, sai
+* do range da timeline.
+*************************************************************************
+* param (E): QDragLeaveEvent * - Evento de DragLeave
+*************************************************************************
+* Histórico
+* 29/09/08 - Fabricio Lopes de Souza
+* Criação.
+************************************************************************/
 void QWidgetTimeline::dragLeaveEvent(QDragLeaveEvent *event)
 {
 	// Simpelsmente limpa o cabecalho da timeline
 	Interface_ui->clearTransitionHeader();
+	// E repinta
 	Interface_ui->updateTimeline();
 }
 
-
+/************************************************************************
+* A partir do evento, retorna qual Efeito vai ser aplicado.
+*************************************************************************
+* param (E): QDropEvent* - Evento de Drop
+*************************************************************************
+* return (E): int - ID do efeito a ser aplicado
+*************************************************************************
+* Histórico
+* 29/09/08 - Fabricio Lopes de Souza
+* Criação.
+************************************************************************/
 int QWidgetTimeline::getItemByEvent(QDropEvent *event)
 {
 	int item = -1;
 
+	// Se for do tipo de QTreeItem
 	if (event->mimeData()->hasFormat("application/x-qabstractitemmodeldatalist"))
 	{
 		QByteArray itemData = event->mimeData()->data("application/x-qabstractitemmodeldatalist");
@@ -241,6 +322,19 @@ int QWidgetTimeline::getItemByEvent(QDropEvent *event)
 }
 
 
+/************************************************************************
+* A partir de um ponto, pinta o Header da timeline
+* referente a uma transição.
+*************************************************************************
+* param (E): QPoint - Ponto da transição
+* param (E): int    - 1 para limpar antes de pintar
+*************************************************************************
+* return (E): int - 0 - Pintou / 1 - Nao pintou
+*************************************************************************
+* Histórico
+* 29/09/08 - Fabricio Lopes de Souza
+* Criação.
+************************************************************************/
 int QWidgetTimeline::selectDropTransition(QPoint pointEvent, int clear)
 {
 	unsigned int transitionID;
--- DEV/source/TransitionEdit.cpp
+++ DEV/source/TransitionEdit.cpp
@@ -36,6 +36,15 @@ TransitionEdit::TransitionEdit(QDialog *parent) : QDialog(parent)
 	this->pos_l = 0;
 }
 
+/************************************************************************
+* Define o ID da transicao que sera Editada
+*************************************************************************
+* param (E): unsigned int - ID da transicao
+************************************************************************
+* Histórico
+* 19/10/08 - Fabricio Lopes de Souza
+* Criação.
+************************************************************************/
 void TransitionEdit::setID(unsigned int id)
 {
 	Transition* transition;
@@ -43,21 +52,36 @@ void TransitionEdit::setID(unsigned int id)
 	long pos_l;
 	char pos_cy[10];
 
+	// Pega a transicao desejada
 	transition = &currentProject->transitionList.at(id);
 	vdo = currentProject->getVideo();
 	pos_l = (long)transition->getPosCurrent();
 
 	sprintf(pos_cy, "%ld", pos_l);
 
+	// Escreve na label o label da transicao
 	ui.labelTransitionName->setText(transition->getLabel());
+
+	// Escreve a posicao da transicao
 	ui.labelFramePos->setText(pos_cy);
 
+	// Aponta o ponteiro do Video para a posicao da transicao.
 	vdo->seekFrame(pos_l);
 
+	// Atualiza o preview
 	updatePreview();
 
 }
 
+/************************************************************************
+* Atualiza a label de Preview com o Frame corrente do video.
+*************************************************************************
+* param (E): Nenhum
+************************************************************************
+* Histórico
+* 19/10/08 - Fabricio Lopes de Souza
+* Criação.
+************************************************************************/
 void TransitionEdit::updatePreview()
 {
 
@@ -67,8 +91,10 @@ void TransitionEdit::updatePreview()
 	long pos_l;
 	char pos_cy[10];
 
+	// Pega o Frame corrente
 	frameNew = vdo->getCurrentFrame();
 
+	// Transforma para QImage
 	image = frameNew->IplImageToQImage(&vdo_player->imageData, &vdo_player->imgWidth, &vdo_player->imgHeight);
 
 	QPixmap pix_image = QPixmap::fromImage(*image);
@@ -77,15 +103,26 @@ void TransitionEdit::updatePreview()
 	ui.labelPreview->setScaledContents(true);
 	ui.labelPreview->setPixmap(pix_image); // Pinta a imagem
 
+	// Atualiza a label da posição corrente do video
 	pos_l = (long)vdo->getCurrentPosition();
 	sprintf(pos_cy, "%ld", pos_l);
-
 	ui.labelCurrentPos->setText(pos_cy);
 
 	delete frameNew;
 	delete image;
 }
 
+/************************************************************************
+* Evento do botak "Change".
+* Ativa a flag avisando que o ponto da transição deve ser modificado
+* e salva a posição corrente do Video.
+*************************************************************************
+* param (E): Nenhum
+************************************************************************
+* Histórico
+* 19/10/08 - Fabricio Lopes de Souza
+* Criação.
+************************************************************************/
 void TransitionEdit::on_changeButton_clicked()
 {
 	Video *vdo;
@@ -101,18 +138,49 @@ void TransitionEdit::on_changeButton_clicked()
 	this->change = true;
 }
 
+/************************************************************************
+* Evento do botao "Delete".
+* Ativa a flag avisando que a transição deve ser apagada.
+*************************************************************************
+* param (E): Nenhum
+************************************************************************
+* Histórico
+* 19/10/08 - Fabricio Lopes de Souza
+* Criação.
+************************************************************************/
 void TransitionEdit::on_deleteButton_clicked()
 {
 	this->del = true;
 	this->change = false;
 }
 
+/************************************************************************
+* Evento do botao "Cancel".
+* Ativa a flag avisando que nenhuma alteração deve ser efetivada.
+*************************************************************************
+* param (E): Nenhum
+************************************************************************
+* Histórico
+* 19/10/08 - Fabricio Lopes de Souza
+* Criação.
+************************************************************************/
 void TransitionEdit::on_cancelButton_clicked()
 {
 	this->cancel = true;
 	QDialog::close();
 }
 
+/************************************************************************
+* Evento do botao "Back".
+* Move o ponteiro do Video para o Frame anterior e pede para atualizar
+* o preview
+*************************************************************************
+* param (E): Nenhum
+************************************************************************
+* Histórico
+* 19/10/08 - Fabricio Lopes de Souza
+* Criação.
+************************************************************************/
 void TransitionEdit::on_backButton_clicked()
 {
 	Video *vdo;
@@ -122,6 +190,18 @@ void TransitionEdit::on_backButton_clicked()
 
 	updatePreview();
 }
+
+/************************************************************************
+* Evento do botao "Forward".
+* Move o ponteiro do Video para o Frame posterior e pede para atualizar
+* o preview
+*************************************************************************
+* param (E): Nenhum
+************************************************************************
+* Histórico
+* 19/10/08 - Fabricio Lopes de Souza
+* Criação.
+************************************************************************/
 void TransitionEdit::on_forwardButton_clicked()
 {
 	Video *vdo;
@@ -132,6 +212,16 @@ void TransitionEdit::on_forwardButton_clicked()
 	updatePreview();
 }
 
+/************************************************************************
+* Evento do botao "Ok".
+* Ativa a flag sinalizando que deve efetivar as alterações do usuário.
+*************************************************************************
+* param (E): Nenhum
+************************************************************************
+* Histórico
+* 19/10/08 - Fabricio Lopes de Souza
+* Criação.
+************************************************************************/
 void TransitionEdit::on_okButton_clicked()
 {
 	this->cancel = false;
